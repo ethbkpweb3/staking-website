38468c55183516dab88f3ec8d320f27d61ea8662
Eth.bkp <49158416+ethbkpweb3@users.noreply.github.com>
2024-11-11 04:13:04 +0530

    Update index-RIpuQQFk.js

--- assets/index-RIpuQQFk.js
+++ assets/index-RIpuQQFk.js
@@ -1,4 +1,14 @@
-import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.js";import{S as l}from"./mainStyles-R3YPfcg-.js";const z=t.div`
+import {
+  s as t,
+  L as w,
+  a as h,
+  c as u,
+  j as n,
+  bx as b,
+  bv as j,
+} from "./index-IT1gKaF9.js";
+import { S as l } from "./mainStyles-R3YPfcg-.js";
+const z = t.div`
   background: linear-gradient(
       180deg,
       rgba(5, 86, 48, 1) 25%,
@@ -10,7 +20,8 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
   background-size: cover;
   background-position: center;
   position: relative;
-`,k=t.div`
+`,
+  k = t.div`
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -19,7 +30,8 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
   margin-right: auto;
   justify-content: center;
   padding: 24px 24px;
-`,v=t.h1`
+`,
+  v = t.h1`
   font-family: "Bebas Neue", cursive;
   font-size: 56px;
   color: #fcefde;
@@ -38,11 +50,12 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
     font-size: 36px;
     text-align: center;
   }
-`,y=t.p`
+`,
+  y = t.p`
   font-size: 20px;
   color: #e8e8e8;
-  font-weight: ${({bold:e})=>e?"bold":"none"};
-  text-align: ${({center:e})=>e?"center":"start"};
+  font-weight: ${({ bold: e }) => (e ? "bold" : "none")};
+  text-align: ${({ center: e }) => (e ? "center" : "start")};
 
   @media screen and (max-width: 1300px) {
     font-size: 18px;
@@ -55,11 +68,12 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
   @media screen and (max-width: 480px) {
     font-size: 14px;
   }
-`,r=t.p`
+`,
+  r = t.p`
   font-size: 18px;
   color: #e8e8e8;
-  font-weight: ${({bold:e})=>e?"bold":"none"};
-  text-align: ${({center:e})=>e?"center":"start"};
+  font-weight: ${({ bold: e }) => (e ? "bold" : "none")};
+  text-align: ${({ center: e }) => (e ? "center" : "start")};
 
   @media screen and (max-width: 1300px) {
     font-size: 16px;
@@ -72,23 +86,27 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
   @media screen and (max-width: 480px) {
     font-size: 12px;
   }
-`,p=t.div`
+`,
+  p = t.div`
   display: flex;
   flex-direction: column;
-`,T=t.div`
+`,
+  T = t.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 10px;
   margin-top: 25px;
-`,$=t.div`
+`,
+  $ = t.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   margin-top: 50px;
-`,W=t.button`
+`,
+  W = t.button`
   padding: 4px 8%;
   color: #81451e;
   font-weight: bold;
@@ -124,11 +142,12 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
     border-image: url("/images/nav/navButton.svg") 12 fill;
     color: #81451e;
   }
-`,B=t(w)`
+`,
+  B = t(w)`
   font-size: 18px;
   color: #e8e8e8;
-  font-weight: ${({bold:e})=>e?"bold":"none"};
-  text-align: ${({center:e})=>e?"center":"start"};
+  font-weight: ${({ bold: e }) => (e ? "bold" : "none")};
+  text-align: ${({ center: e }) => (e ? "center" : "start")};
 
   @media screen and (max-width: 1300px) {
     font-size: 16px;
@@ -141,7 +160,8 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
   @media screen and (max-width: 480px) {
     font-size: 12px;
   }
-`,A=t.div`
+`,
+  A = t.div`
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -154,7 +174,8 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
     justify-content: start;
     align-items: start;
   }
-`,S=t.table`
+`,
+  S = t.table`
   display: flex;
   flex-direction: column;
   border: 10px solid;
@@ -163,23 +184,25 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
   box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px,
     rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px,
     rgba(0, 0, 0, 0.07) 0px 16px 16px;
-`,m=t.tr`
+`,
+  m = t.tr`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: flex-start;
   align-items: center;
-  border-bottom: ${({noBorder:e})=>e?"none":"1px solid #707070"};
+  border-bottom: ${({ noBorder: e }) => (e ? "none" : "1px solid #707070")};
   overflow-wrap: break-word;
   width: 100%;
-`,d=t.th`
+`,
+  d = t.th`
   flex: 1;
   font-size: 18px;
   font-weight: bold;
   color: #81451e;
   text-align: start;
   padding: 2.5px 10px;
-  border-right: ${({noBorder:e})=>e?"none":"1px solid #707070"};
+  border-right: ${({ noBorder: e }) => (e ? "none" : "1px solid #707070")};
 
   @media screen and (max-width: 1300px) {
     font-size: 14px;
@@ -192,12 +215,13 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
   @media screen and (max-width: 480px) {
     font-size: 11px;
   }
-`,s=t.td`
+`,
+  s = t.td`
   flex: 1;
   font-size: 16px;
   padding: 10px;
-  word-break: ${({doBreak:e})=>e?"break-all":"normal"};
-  border-right: ${({noBorder:e})=>e?"none":"1px solid #707070"};
+  word-break: ${({ doBreak: e }) => (e ? "break-all" : "normal")};
+  border-right: ${({ noBorder: e }) => (e ? "none" : "1px solid #707070")};
 
   @media screen and (max-width: 1300px) {
     font-size: 14px;
@@ -213,7 +237,8 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
     font-size: 11px;
     padding: 5px;
   }
-`;t.div`
+`;
+t.div`
   display: flex;
   flex: 1;
   align-items: center;
@@ -222,11 +247,12 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
   @media screen and (max-width: 480px) {
     flex-direction: column;
   }
-`;t.p`
+`;
+t.p`
   font-size: 16px;
   font-weight: bold;
   padding: 10px;
-  word-break: ${({doBreak:e})=>e?"break-all":"normal"};
+  word-break: ${({ doBreak: e }) => (e ? "break-all" : "normal")};
 
   @media screen and (max-width: 1300px) {
     font-size: 14px;
@@ -242,4 +268,113 @@ import{s as t,L as w,a as h,c as u,j as n,bx as b,bv as j}from"./index-IT1gKaF9.
     font-size: 11px;
     padding: 5px;
   }
-`;const C=()=>{const{t:e}=h(),{coinStakeInfo:i}=u(),f=60*60*24*365;function x(a){return a?a.toLocaleString():0}const g=a=>(f*i.numerator[a]/i.denominator[a]/i.timeUnit*i.ratioAmount[a]+i.ratioAmount[a]-i.ratioAmount[a])/i.ratioAmount[a]*100;return n.jsx(A,{children:n.jsxs(S,{children:[n.jsx("thead",{children:n.jsxs(m,{children:[n.jsx(d,{children:e("table.coin.header1")}),n.jsx(d,{children:e("table.coin.header2")}),n.jsx(d,{noBorder:!0,children:e("table.coin.header3")})]})}),n.jsx("tbody",{children:i.ratioAmount.map((a,o)=>{const c=g(o);return n.jsxs(m,{noBorder:i.ratioAmount.length<=o+1,children:[n.jsx(s,{children:x(a)}),n.jsx(s,{children:i.ratioAmount[o+1]?x(i.ratioAmount[o+1]-1):"∞"}),n.jsx(s,{noBorder:!0,children:`${c.toFixed(0)}%`})]},o)})})]})})},P=()=>{const{t:e}=h(),i=b();return n.jsxs(z,{id:"howCoin",children:[n.jsx(l,{}),n.jsxs(k,{children:[n.jsx(v,{children:e("howItWorks.coin.header")}),n.jsxs(p,{style:{marginTop:"50px"},children:[n.jsx(y,{center:!0,bold:!0,children:e("howItWorks.coin.smallHeader1")}),n.jsx(r,{center:!0,children:e("howItWorks.coin.text1")})]}),n.jsx(p,{style:{marginTop:"25px"},children:n.jsx(r,{center:!0,children:n.jsxs(j,{i18nKey:"howItWorks.coin.text2",children:["Link",n.jsx(B,{to:"/coin/how",children:"link"})]})})}),n.jsxs($,{children:[n.jsx(C,{}),n.jsxs(T,{children:[n.jsx(r,{center:!0,children:e("howItWorks.coin.calcText")}),n.jsx(W,{onClick:()=>i("/coin/calculator"),children:e("howItWorks.coin.calcButton")})]})]})]}),n.jsx(l,{})]})};export{P as default};
+`;
+const C = () => {
+    const { t: e } = h(),
+      { coinStakeInfo: i } = u(),
+      f = 60 * 60 * 24 * 365;
+    function x(a) {
+      return a ? a.toLocaleString() : 0;
+    }
+    const g = (a) =>
+      ((((f * i.numerator[a]) / i.denominator[a] / i.timeUnit) *
+        i.ratioAmount[a] +
+        i.ratioAmount[a] -
+        i.ratioAmount[a]) /
+        i.ratioAmount[a]) *
+      100;
+    return n.jsx(A, {
+      children: n.jsxs(S, {
+        children: [
+          n.jsx("thead", {
+            children: n.jsxs(m, {
+              children: [
+                n.jsx(d, { children: e("table.coin.header1") }),
+                n.jsx(d, { children: e("table.coin.header2") }),
+                n.jsx(d, { noBorder: !0, children: e("table.coin.header3") }),
+              ],
+            }),
+          }),
+          n.jsx("tbody", {
+            children: i.ratioAmount.map((a, o) => {
+              const c = g(o);
+              return n.jsxs(
+                m,
+                {
+                  noBorder: i.ratioAmount.length <= o + 1,
+                  children: [
+                    n.jsx(s, { children: x(a) }),
+                    n.jsx(s, {
+                      children: i.ratioAmount[o + 1]
+                        ? x(i.ratioAmount[o + 1] - 1)
+                        : "∞",
+                    }),
+                    n.jsx(s, { noBorder: !0, children: `${c.toFixed(0)}%` }),
+                  ],
+                },
+                o
+              );
+            }),
+          }),
+        ],
+      }),
+    });
+  },
+  P = () => {
+    const { t: e } = h(),
+      i = b();
+    return n.jsxs(z, {
+      id: "howCoin",
+      children: [
+        n.jsx(l, {}),
+        n.jsxs(k, {
+          children: [
+            n.jsx(v, { children: e("howItWorks.coin.header") }),
+            n.jsxs(p, {
+              style: { marginTop: "50px" },
+              children: [
+                n.jsx(y, {
+                  center: !0,
+                  bold: !0,
+                  children: e("howItWorks.coin.smallHeader1"),
+                }),
+                n.jsx(r, { center: !0, children: e("howItWorks.coin.text1") }),
+              ],
+            }),
+            n.jsx(p, {
+              style: { marginTop: "25px" },
+              children: n.jsx(r, {
+                center: !0,
+                children: n.jsxs(j, {
+                  i18nKey: "howItWorks.coin.text2",
+                  children: [
+                    "Link",
+                    n.jsx(B, { to: "/coin/how", children: "link" }),
+                  ],
+                }),
+              }),
+            }),
+            n.jsxs($, {
+              children: [
+                n.jsx(C, {}),
+                n.jsxs(T, {
+                  children: [
+                    n.jsx(r, {
+                      center: !0,
+                      children: e("howItWorks.coin.calcText"),
+                    }),
+                    n.jsx(W, {
+                      onClick: () => i("/coin/calculator"),
+                      children: e("howItWorks.coin.calcButton"),
+                    }),
+                  ],
+                }),
+              ],
+            }),
+          ],
+        }),
+        n.jsx(l, {}),
+      ],
+    });
+  };
+export { P as default };
